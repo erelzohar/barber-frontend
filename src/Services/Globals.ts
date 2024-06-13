f701851ba05b3236c14017c7e321c2666929ffdd
erelzohar <zoharerel@gmail.com>
2024-06-13 15:15:59 +0300

    globals fix

--- src/Services/Globals.ts
+++ src/Services/Globals.ts
@@ -15,12 +15,12 @@ class DevelopmentGlobals extends Globals {
 }
 
 class ProductionGlobals extends Globals {
-    public linesUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/line";
-    public adminUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/admin";
-    public loginUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/auth/login";
-    public smsUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/sms";
-    public ordersUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/orders";
-    public messagesUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/message";
+    public linesUrl = "https://barber-backend-app-308bdd2961da.herokuapp.com/api/line";
+    public adminUrl = "https://barber-backend-app-308bdd2961da.herokuapp.com/api/admin";
+    public loginUrl = "https://barber-backend-app-308bdd2961da.herokuapp.com/api/auth/login";
+    public smsUrl = "https://barber-backend-app-308bdd2961da.herokuapp.com/api/sms";
+    public ordersUrl = "https://barber-backend-app-308bdd2961da.herokuapp.com/api/orders";
+    public messagesUrl = "https://barber-backend-app-308bdd2961da.herokuapp.com/api/message";
 }
 
 const globals = process.env.NODE_ENV === "production" ? new ProductionGlobals() : new DevelopmentGlobals();
