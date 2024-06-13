78fc95d0fbe6c4b3e769775e11a0d5a565f1e0bc
erelzohar <zoharerel@gmail.com>
2024-06-13 13:11:26 +0300

    first commit

--- src/Services/Globals.ts
+++ src/Services/Globals.ts
@@ -15,12 +15,12 @@ class DevelopmentGlobals extends Globals {
 }
 
 class ProductionGlobals extends Globals {
-    public registerUrl = "https://polar-garden-74885-12cdd1b0d0eb.herokuapp.com/api/auth/register";
-    public loginUrl = "https://polar-garden-74885-12cdd1b0d0eb.herokuapp.com/api/auth/login";
-    public productsUrl = "https://polar-garden-74885-12cdd1b0d0eb.herokuapp.com/api/products";
-    public ordersUrl = "https://polar-garden-74885-12cdd1b0d0eb.herokuapp.com/api/orders";
-    public messagesUrl = "https://polar-garden-74885-12cdd1b0d0eb.herokuapp.com/api/message";
-
+    public linesUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/line";
+    public adminUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/admin";
+    public loginUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/auth/login";
+    public smsUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/sms";
+    public ordersUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/orders";
+    public messagesUrl = "http://barber-backend-app-308bdd2961da.herokuapp.com/api/message";
 }
 
 const globals = process.env.NODE_ENV === "production" ? new ProductionGlobals() : new DevelopmentGlobals();
